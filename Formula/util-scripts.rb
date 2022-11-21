bc7d790599f6859d80291eaf7e254d8863580f22
Christopher Maahs <christopher.maahs@alteryx.com>
2022-11-21 14:17:04 -0600

    Adding a new homebrew location for util-scripts

---
+++ Formula/util-scripts.rb
@@ -0,0 +1,334 @@
+# typed: false
+# frozen_string_literal: true
+
+class UtilScripts < Formula
+  desc "This is a collection of general utility scripts"
+  homepage "https://github.com/cmaahs/homebrew-admin-scripts"
+  url "https://github.com/cmaahs/homebrew-admin-scripts.git"
+  version "0.0.95"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/cmaahs/homebrew-admin-scripts/releases/download/v0.0.95/homebrew-admin-scripts.tar.gz"
+      sha256 "2c2531a34a83d54f0b7cc3636db96133c16f234b3a8122f4edcfd3b0233d9092"
+
+      def install
+        bin.install "bin/check-docker-tag"
+        bin.install "bin/bash_256.sh"
+        bin.install "bin/bash_color_ls.sh"
+        bin.install "bin/bash_colors.sh"
+        bin.install "bin/color.zsh"
+        bin.install "bin/colorgrid.sh"
+        bin.install "bin/k8s-vault-functions.sh"
+        bin.install "bin/k8s_creds_functions.sh"
+        bin.install "bin/k8s_postgres_functions.sh"
+        bin.install "bin/k8s_kubeconfig_functions.sh"
+        bin.install "bin/update-az-dns"
+        bin.install "bin/otp"
+        bin.install "bin/otp_basic"
+        bin.install "bin/switch_jira.sh"
+        bin.install "bin/create_aws_environments.sh"
+        bin.install "bin/create_azure_environments.sh"
+        bin.install "bin/create_dns_yaml.sh"
+        bin.install "bin/date_functions.sh"
+        bin.install "bin/destroy_aws_environments.sh"
+        bin.install "bin/destroy_azure_environments.sh"
+        bin.install "bin/get-jenkinsjobs"
+        bin.install "bin/get-terraformversion"
+        bin.install "bin/get-operator-sdk"
+        bin.install "bin/kurls-source.sh"
+        bin.install "bin/launch-splicectl-api.sh"
+        bin.install "bin/ls-window-title"
+        bin.install "bin/mk-window-title"
+        bin.install "bin/rm-window-title"
+        bin.install "bin/k8s_list_environments.sh"
+        bin.install "bin/log-path.sh"
+        bin.install "bin/iterm-set-titles.sh"
+        bin.install "bin/roll_version.sh"
+        bin.install "bin/order-manifest.ps1"
+        bin.install "bin/split-manifest.ps1"
+        bin.install "bin/prstat"
+        bin.install "bin/sqlshell_functions.sh"
+        bin.install "bin/vault_functions.sh"
+        bin.install "bin/xkcd"
+        bin.install "bin/new-bash-script"
+        bin.install "bin/get-gke-versions"
+        bin.install "bin/get-jsonui"
+        bin.install "bin/worktree_functions.sh"
+        bin.install "bin/git_functions.sh"
+        bin.install "bin/new-retro"
+        bin.install "bin/new-hackathon"
+        bin.install "bin/start-bind9"
+        bin.install "bin/stop-bind9"
+        bin.install "bin/get-github-pat"
+        bin.install "bin/get-gitlab-api-pat"
+        bin.install "bin/helm_ecr_functions.sh"
+        bin.install "bin/bw_functions.sh"
+        bin.install "bin/bash_functions.sh"
+        bin.install "bin/git-fetch-status-pull"
+        bin.install "bin/gcp_functions.sh"
+        bin.install "bin/gcp-switch-account"
+        bin.install "bin/decolorize"
+        bin.install "bin/get-riverlevels"
+        bin.install "bin/river-prompt"
+        bin.install "bin/show-riverlevels"
+        bin.install "bin/vim_multi_commands.sh"
+        bin.install "bin/vault-token-to-clipboard"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/cmaahs/homebrew-admin-scripts/releases/download/v0.0.95/homebrew-admin-scripts.tar.gz"
+      sha256 "2c2531a34a83d54f0b7cc3636db96133c16f234b3a8122f4edcfd3b0233d9092"
+
+      def install
+        bin.install "bin/check-docker-tag"
+        bin.install "bin/bash_256.sh"
+        bin.install "bin/bash_color_ls.sh"
+        bin.install "bin/bash_colors.sh"
+        bin.install "bin/color.zsh"
+        bin.install "bin/colorgrid.sh"
+        bin.install "bin/k8s-vault-functions.sh"
+        bin.install "bin/k8s_creds_functions.sh"
+        bin.install "bin/k8s_postgres_functions.sh"
+        bin.install "bin/k8s_kubeconfig_functions.sh"
+        bin.install "bin/update-az-dns"
+        bin.install "bin/otp"
+        bin.install "bin/otp_basic"
+        bin.install "bin/switch_jira.sh"
+        bin.install "bin/create_aws_environments.sh"
+        bin.install "bin/create_azure_environments.sh"
+        bin.install "bin/create_dns_yaml.sh"
+        bin.install "bin/date_functions.sh"
+        bin.install "bin/destroy_aws_environments.sh"
+        bin.install "bin/destroy_azure_environments.sh"
+        bin.install "bin/get-jenkinsjobs"
+        bin.install "bin/get-terraformversion"
+        bin.install "bin/get-operator-sdk"
+        bin.install "bin/kurls-source.sh"
+        bin.install "bin/launch-splicectl-api.sh"
+        bin.install "bin/ls-window-title"
+        bin.install "bin/mk-window-title"
+        bin.install "bin/rm-window-title"
+        bin.install "bin/k8s_list_environments.sh"
+        bin.install "bin/log-path.sh"
+        bin.install "bin/iterm-set-titles.sh"
+        bin.install "bin/roll_version.sh"
+        bin.install "bin/order-manifest.ps1"
+        bin.install "bin/split-manifest.ps1"
+        bin.install "bin/prstat"
+        bin.install "bin/sqlshell_functions.sh"
+        bin.install "bin/vault_functions.sh"
+        bin.install "bin/xkcd"
+        bin.install "bin/new-bash-script"
+        bin.install "bin/get-gke-versions"
+        bin.install "bin/get-jsonui"
+        bin.install "bin/worktree_functions.sh"
+        bin.install "bin/git_functions.sh"
+        bin.install "bin/new-retro"
+        bin.install "bin/new-hackathon"
+        bin.install "bin/start-bind9"
+        bin.install "bin/stop-bind9"
+        bin.install "bin/get-github-pat"
+        bin.install "bin/get-gitlab-api-pat"
+        bin.install "bin/helm_ecr_functions.sh"
+        bin.install "bin/bw_functions.sh"
+        bin.install "bin/bash_functions.sh"
+        bin.install "bin/git-fetch-status-pull"
+        bin.install "bin/gcp_functions.sh"
+        bin.install "bin/gcp-switch-account"
+        bin.install "bin/decolorize"
+        bin.install "bin/get-riverlevels"
+        bin.install "bin/river-prompt"
+        bin.install "bin/show-riverlevels"
+        bin.install "bin/vim_multi_commands.sh"
+        bin.install "bin/vault-token-to-clipboard"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/cmaahs/homebrew-admin-scripts/releases/download/v0.0.95/homebrew-admin-scripts.tar.gz"
+      sha256 "2c2531a34a83d54f0b7cc3636db96133c16f234b3a8122f4edcfd3b0233d9092"
+
+      def install
+        bin.install "bin/check-docker-tag"
+        bin.install "bin/bash_256.sh"
+        bin.install "bin/bash_color_ls.sh"
+        bin.install "bin/bash_colors.sh"
+        bin.install "bin/color.zsh"
+        bin.install "bin/colorgrid.sh"
+        bin.install "bin/k8s-vault-functions.sh"
+        bin.install "bin/k8s_creds_functions.sh"
+        bin.install "bin/k8s_postgres_functions.sh"
+        bin.install "bin/k8s_kubeconfig_functions.sh"
+        bin.install "bin/update-az-dns"
+        bin.install "bin/otp"
+        bin.install "bin/otp_basic"
+        bin.install "bin/switch_jira.sh"
+        bin.install "bin/create_aws_environments.sh"
+        bin.install "bin/create_azure_environments.sh"
+        bin.install "bin/create_dns_yaml.sh"
+        bin.install "bin/date_functions.sh"
+        bin.install "bin/destroy_aws_environments.sh"
+        bin.install "bin/destroy_azure_environments.sh"
+        bin.install "bin/get-jenkinsjobs"
+        bin.install "bin/get-terraformversion"
+        bin.install "bin/get-operator-sdk"
+        bin.install "bin/kurls-source.sh"
+        bin.install "bin/launch-splicectl-api.sh"
+        bin.install "bin/ls-window-title"
+        bin.install "bin/mk-window-title"
+        bin.install "bin/rm-window-title"
+        bin.install "bin/k8s_list_environments.sh"
+        bin.install "bin/log-path.sh"
+        bin.install "bin/iterm-set-titles.sh"
+        bin.install "bin/roll_version.sh"
+        bin.install "bin/order-manifest.ps1"
+        bin.install "bin/split-manifest.ps1"
+        bin.install "bin/prstat"
+        bin.install "bin/sqlshell_functions.sh"
+        bin.install "bin/vault_functions.sh"
+        bin.install "bin/xkcd"
+        bin.install "bin/new-bash-script"
+        bin.install "bin/get-gke-versions"
+        bin.install "bin/get-jsonui"
+        bin.install "bin/worktree_functions.sh"
+        bin.install "bin/git_functions.sh"
+        bin.install "bin/new-retro"
+        bin.install "bin/new-hackathon"
+        bin.install "bin/start-bind9"
+        bin.install "bin/stop-bind9"
+        bin.install "bin/get-github-pat"
+        bin.install "bin/get-gitlab-api-pat"
+        bin.install "bin/helm_ecr_functions.sh"
+        bin.install "bin/bw_functions.sh"
+        bin.install "bin/bash_functions.sh"
+        bin.install "bin/git-fetch-status-pull"
+        bin.install "bin/gcp_functions.sh"
+        bin.install "bin/gcp-switch-account"
+        bin.install "bin/decolorize"
+        bin.install "bin/get-riverlevels"
+        bin.install "bin/river-prompt"
+        bin.install "bin/show-riverlevels"
+        bin.install "bin/vim_multi_commands.sh"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/cmaahs/homebrew-admin-scripts/releases/download/v0.0.95/homebrew-admin-scripts.tar.gz"
+      sha256 "2c2531a34a83d54f0b7cc3636db96133c16f234b3a8122f4edcfd3b0233d9092"
+
+      def install
+        bin.install "bin/check-docker-tag"
+        bin.install "bin/bash_256.sh"
+        bin.install "bin/bash_color_ls.sh"
+        bin.install "bin/bash_colors.sh"
+        bin.install "bin/color.zsh"
+        bin.install "bin/colorgrid.sh"
+        bin.install "bin/k8s-vault-functions.sh"
+        bin.install "bin/k8s_creds_functions.sh"
+        bin.install "bin/k8s_postgres_functions.sh"
+        bin.install "bin/k8s_kubeconfig_functions.sh"
+        bin.install "bin/update-az-dns"
+        bin.install "bin/otp"
+        bin.install "bin/otp_basic"
+        bin.install "bin/switch_jira.sh"
+        bin.install "bin/create_aws_environments.sh"
+        bin.install "bin/create_azure_environments.sh"
+        bin.install "bin/create_dns_yaml.sh"
+        bin.install "bin/date_functions.sh"
+        bin.install "bin/destroy_aws_environments.sh"
+        bin.install "bin/destroy_azure_environments.sh"
+        bin.install "bin/get-jenkinsjobs"
+        bin.install "bin/get-terraformversion"
+        bin.install "bin/get-operator-sdk"
+        bin.install "bin/kurls-source.sh"
+        bin.install "bin/launch-splicectl-api.sh"
+        bin.install "bin/ls-window-title"
+        bin.install "bin/mk-window-title"
+        bin.install "bin/rm-window-title"
+        bin.install "bin/k8s_list_environments.sh"
+        bin.install "bin/log-path.sh"
+        bin.install "bin/iterm-set-titles.sh"
+        bin.install "bin/roll_version.sh"
+        bin.install "bin/order-manifest.ps1"
+        bin.install "bin/split-manifest.ps1"
+        bin.install "bin/prstat"
+        bin.install "bin/sqlshell_functions.sh"
+        bin.install "bin/vault_functions.sh"
+        bin.install "bin/xkcd"
+        bin.install "bin/new-bash-script"
+        bin.install "bin/get-gke-versions"
+        bin.install "bin/get-jsonui"
+        bin.install "bin/worktree_functions.sh"
+        bin.install "bin/git_functions.sh"
+        bin.install "bin/new-retro"
+        bin.install "bin/new-hackathon"
+        bin.install "bin/start-bind9"
+        bin.install "bin/stop-bind9"
+        bin.install "bin/get-github-pat"
+        bin.install "bin/get-gitlab-api-pat"
+        bin.install "bin/helm_ecr_functions.sh"
+        bin.install "bin/bw_functions.sh"
+        bin.install "bin/bash_functions.sh"
+        bin.install "bin/git-fetch-status-pull"
+        bin.install "bin/gcp_functions.sh"
+        bin.install "bin/gcp-switch-account"
+        bin.install "bin/decolorize"
+        bin.install "bin/get-riverlevels"
+        bin.install "bin/river-prompt"
+        bin.install "bin/show-riverlevels"
+        bin.install "bin/vim_multi_commands.sh"
+      end
+    end
+  end
+
+  def caveats; <<~EOS
+    The use of k8s-vault-functions.sh, k8s_cred_functions.sh, sqlshell_functions.sh,
+    and switch_vault.sh all require a "source {script.sh}" line in the startup
+    profiles, .bashrc/.zshrc
+      git_functions.sh contains the following functions:
+        - git-add-modified
+        - config-add-modified
+          * used to add all the "modified" files, config is the dotfiles alias
+      gcp_functions.sh contains teh following functions:
+        - gcp-switch-project
+          * used to switch projects using an ENV variable
+      vim_multi_commands.sh contains th following functions:
+        - vi-all-extension
+          * used to edit ALL items of a single extension, recursively
+      sqlshell_functions.sh contains the following function:
+        - connect-sqlshell
+      k8s-vault-function.sh contains the following functions:
+        - connect_k8s_vault
+        - disconnect_k8s_vault
+      k8s_cred_functions.sh contains the following functions:
+        - get-aws-creds
+        - get-az-creds
+        - get-gcp-creds
+        k8s_list_environments.sh contains the following functions:
+        - get-aws-environments
+        - get-az-environments
+        - get-gcp-environments
+        vault_functions.sh contains the following function:
+        - switch-vault
+        - auth-vault
+        switch_jira.sh contains the following function:
+        - switch-jira
+      otp and otp_basic are used with oauth-toolkit
+        - otp uses hasicorp vault or mac os keyring for sensitive data storage
+        - otp_basic stores sensitive data in ~/.otpkeys
+          - if you choose otp_basic, just re-link /usr/local/bin/otp to the otp_basic version.
+      iterm-set-titles.sh contains the following functions:
+        - set-title-window
+        - set-title-tab
+        - set-tab-title-pwd
+      worktree_functions.sh contains the following functions:
+        - mkwt <branchname> (branchname needs to be in the form of JIRANUM/description)
+        - cdwt (prompts for a worktree to change to)
+        - cdwtp (changes to the parent git directory)
+        - rmwt (prompts for a worktree to remove, and prompt to remove empty parent)
+  EOS
+  end
+end
+
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # homebrew-tap
+
 Homebrew TAP for use in deploying apps/scripts via brew
+
+
+## UtilScripts
+
+Collection of brew installable administrative scripts for making my daily life easier
+
+### Scripts and their respective Formula
+
+#### Formula util-scripts
+
+`brew install cmaahs/util-scripts`
+
+- bin/check-docker-tag
+- bin/bash_256.sh
+- bin/bash_color_ls.sh
+- bin/bash_colors.sh
+- bin/color.zsh
+- bin/colorgrid.sh
+- bin/k8s-vault-functions.sh
+- bin/k8s_creds_functions.sh
+- bin/k8s_postgres_functions.sh
+- bin/k8s_kubeconfig_functions.sh
+- bin/update-az-dns
+- bin/otp
+- bin/otp_basic
+- bin/switch_jira.sh
+- bin/create_aws_environments.sh
+- bin/create_azure_environments.sh
+- bin/create_dns_yaml.sh
+- bin/date_functions.sh
+- bin/destroy_aws_environments.sh
+- bin/destroy_azure_environments.sh
+- bin/get-jenkinsjobs
+- bin/get-terraformversion
+- bin/get-operator-sdk
+- bin/kurls-source.sh
+- bin/launch-splicectl-api.sh
+- bin/ls-window-title
+- bin/mk-window-title
+- bin/rm-window-title
+- bin/k8s_list_environments.sh
+- bin/log-path.sh
+- bin/iterm-set-titles.sh
+- bin/roll_version.sh
+- bin/order-manifest.ps1
+- bin/split-manifest.ps1
+- bin/prstat
+- bin/sqlshell_functions.sh
+- bin/vault_functions.sh
+- bin/xkcd
+- bin/new-bash-script
+- bin/get-gke-versions
+- bin/get-jsonui
+- bin/worktree_functions.sh
+- bin/git_functions.sh
+- bin/new-retro
+- bin/new-hackathon
+- bin/start-bind9
+- bin/stop-bind9
+- bin/get-github-pat
+- bin/get-gitlab-api-pat
+- bin/helm_ecr_functions.sh
+- bin/bw_functions.sh
+- bin/bash_functions.sh
+- bin/git-fetch-status-pull
+- bin/gcp_functions.sh
+- bin/gcp-switch-account
+- bin/decolorize
+- bin/get-riverlevels
+- bin/river-prompt
+- bin/show-riverlevels
+- bin/vim_multi_commands.sh
+- bin/vault-token-to-clipboard
+
